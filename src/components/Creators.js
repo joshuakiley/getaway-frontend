af83e28ed76cedf909c85a2e0faa5ca5dc5301d1
Joshua Kiley <joshua.kiley.dev@gmail.com>
2019-11-21 17:32:33 -0500

    Finish basic functionality of navbar and the slidenav sliding out on mobile view

---
+++ src/components/Creators.js
@@ -0,0 +1,23 @@
+//==============================
+//       COMPONENTS
+//==============================
+import React, { Component } from "react";
+
+class Creators extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ""
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>hey</h1>
+      </div>
+    );
+  }
+}
+
+export default Creators;
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@
 //       COMPONENTS
 //==============================
 import React, { Component } from "react";
+import Creators from "./Creators.js";
 
 //==============================
 //       DEPENDENCIES
@@ -30,6 +31,9 @@ class Main extends Component {
               <a href="#" className="brand-logo">
                 Logo
               </a>
+              <a href="#" data-target="slide-out" className="sidenav-trigger">
+                <i className="material-icons">menu</i>
+              </a>
               <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li>
                   <a href="#">Home</a>
@@ -41,16 +45,43 @@ class Main extends Component {
                   <a href="#">Ticket Prices</a>
                 </li>
                 <li>
-                  <a href="#">Dev Team</a>
+                  <Link to="/creators">Creators</Link>
                 </li>
               </ul>
             </div>
           </nav>
-          <ul></ul>
+          <ul id="slide-out" className="sidenav">
+            <li>
+              <div className="user-view">
+                <a href="#">
+                  <img className="circle" src="/images/download.jpeg" />
+                </a>
+
+                <a href="#">
+                  <span className="white-text name">Bulbasaur</span>
+                </a>
+
+                <a href="#">
+                  <span className="white-text email">email</span>
+                </a>
+              </div>
+            </li>
+            <li>
+              <a href="#" className="waves-effect">
+                <i className="material-icons">home</i>Home
+              </a>
+            </li>
+            <li>
+              <div className="divider"></div>
+            </li>
+            <li>
+              <Link to="/creators">Creators</Link>
+            </li>
+          </ul>
         </header>
         <main>
           <h1></h1>
-          {/* add here */}
+          <Route path="/creators" exact component={Creators} />
         </main>
       </Router>
     );
