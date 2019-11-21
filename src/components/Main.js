e1908d9b63dfea5fc85c847a4eb8708ccc5d3a70
Joshua Kiley <joshua.kiley.dev@gmail.com>
2019-11-21 14:32:16 -0500

    Add navbar template, uncomplete

---
+++ src/components/Main.js
@@ -0,0 +1,94 @@
+//==============================
+//       COMPONENTS
+//==============================
+import React, { Component } from "react";
+
+//==============================
+//       DEPENDENCIES
+//==============================
+// import axios from "axios";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ""
+    };
+  }
+
+  componentDidMount() {
+    console.log("Main Mounted");
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <nav className="nav">
+            <div className="nav-wrapper">
+              <a href="#" className="brand-logo center">
+                Logo
+              </a>
+              <a
+                href="#"
+                id="menu"
+                data-target="slide-out"
+                className="sidenav-trigger"
+              >
+                <i className="material-icons">menu</i>
+              </a>
+            </div>
+          </nav>
+          <ul id="slide-out" className="sidenav">
+            <li>
+              <div className="user-view"></div>
+              <div className="logo center"></div>
+              <div className="brand-logo"></div>
+              <a href="#name">
+                <span className="white-text name">USER</span>
+              </a>
+              <a href="#budget">
+                <span>later add total</span>
+              </a>
+            </li>
+            <li>
+              <a>
+                <i className="material-icons">home</i> HOME
+              </a>
+            </li>
+            <li>
+              <ul className="collapsible collapsible-accordian">
+                <li>
+                  <a
+                    href="#"
+                    className="collapsible-header waves-effect waves-red"
+                  >
+                    PAY PERIODS
+                  </a>
+                  <div className="collapsible-body">
+                    <ul>
+                      <li>
+                        <a href="/payperiods/new">
+                          <i className="material-icons">create_new_folder</i>NEW
+                          PAY PERIOD
+                        </a>
+                      </li>
+
+                      <li>
+                        <a href="/payperiod">
+                          <i className="material-icons">folder</i>date
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Main;
